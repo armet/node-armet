1e0995bce511ec5425b84db42ba620f22679cca1
Ryan Leckey <leckey.ryan@gmail.com>
2015-06-25 17:29:12 -0700

    Add config for testing purposes

---
+++ config/default.js
@@ -0,0 +1,19 @@
+module.exports = {
+  bardo: {
+    db: {
+      database: "armet",
+      host: "localhost",
+      user: "postgres",
+      // password: null,
+      port: 5432
+    },
+    log: {
+      level: "trace"
+    }
+  },
+  armet: {
+    log: {
+      level: "info"
+    }
+  }
+}
