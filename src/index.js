9b39aceb4ea156ba7ba86cd1ac44c2427cd55c15
Ryan Leckey <leckey.ryan@gmail.com>
2015-07-06 14:05:26 -0700

    Add mount to export list on index

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import config from "./config"
 import * as util_ from "./util"
 
 export let Resource = resource.Resource
+export let mount = resource.mount
 export let configure = config.configure
 export let run = server.run
 export let util = util_
 
 export default {
   Resource,
+  mount,
   configure,
   run,
   util
