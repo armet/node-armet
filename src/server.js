5fc92e587b627236261707138ee1cfcc8d054cde
Ryan Leckey <leckey.ryan@gmail.com>
2015-07-06 16:02:00 -0700

    Add common middleware

--- src/server.js
+++ src/server.js
@@ -6,6 +6,47 @@ import config from "./config"
 import log from "./log"
 import {db} from "bardo"
 
+// TODO: Should this move elsewhere?
+function parseObjectReference(req, res, next) {
+  if (req.body == null || Object.keys(req.body).length === 0) {
+    // Body is empty
+    return next()
+  }
+
+  function explode(segment) {
+    if (segment.indexOf("[") === 0) {
+      return segment.slice(1, segment.length - 1)
+    }
+
+    return segment
+  }
+
+  const pattern = /([^\[\]]+)|\[([^\[\]]+?)\]/g
+  for (let key of Object.keys(req.body)) {
+    let ctx = req.body
+    let value = req.body[key]
+    let match = key.match(pattern)
+    if (match != null && match.length >= 2) {
+      // Remove the [..] around the segments
+      match = match.map(explode)
+
+      for (let segment of match.slice(0, match.length - 1)) {
+        // Ensure we have an object and forward the context
+        if (!_.has(ctx, segment)) ctx[segment] = {}
+        ctx = ctx[segment]
+      }
+
+      // Set the value on the inner-most context
+      ctx[match[match.length - 1]] = value
+
+      // Remove the now parsed key
+      delete req.body[key]
+    }
+  }
+
+  return next()
+}
+
 var server = null
 export function get() {
   if (server == null) {
@@ -13,6 +54,21 @@ export function get() {
     server = restify.createServer({
       name: config.get("name"),
     })
+
+    // TODO: Re-direct on case mismatch or trailing slash mismatch
+
+    // Establish middleware (built-in)
+    server.use(restify.acceptParser(server.acceptable))
+    server.use(restify.queryParser())
+    server.use(restify.authorizationParser())
+    server.use(restify.dateParser())
+    server.use(restify.bodyParser({
+      maxBodySize: 0,
+      mapParams: false,
+    }))
+
+    // Setup a body parser to explode field references as x[y][z] to x.y.z
+    server.use(parseObjectReference)
   }
 
   return server
